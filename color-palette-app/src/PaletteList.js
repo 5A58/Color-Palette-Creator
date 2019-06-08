8024904f19509b323dcc30b2cd4670aa1a250c8c
7-Zip <mattkorn129@gmail.com>
2019-06-08 17:09:15 -0400

    Added CSS-in-JS from Material UI and completed basic styling for MiniPalette

--- color-palette-app/src/PaletteList.js
+++ color-palette-app/src/PaletteList.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
+import MiniPalette from './MiniPalette';
 import {Link} from 'react-router-dom';
 
 class PaletteList extends Component {
     render() {
         const {palettes} = this.props;
         const miniPalettes = palettes.map(palette => (
-            <p>
-                <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-            </p>
+            <MiniPalette {...palette}/>
         ));
         return (
             <div>
