41a2e3781d8cc73a5fdb865b18be902b289be29c
7-Zip <mattkorn129@gmail.com>
2019-06-08 16:01:51 -0400

    Fixed routes so palettes appear

--- color-palette-app/src/App.js
+++ color-palette-app/src/App.js
@@ -5,15 +5,23 @@ import seedColors from './seedColors';
 import {generatePalette} from './ColorHelpers';
 
 class App extends Component {
+    findPalette(id) {
+        return seedColors.find((palette) => {
+           return palette.id === id
+        });
+    }
+
     render() {
         return (
             <Switch>
                 <Route exact path={"/"} render={() => <h1>Palette list goes here</h1>}/>
-                <Route exact path={"/palette/:id"} render={() => <h1>Individual Palette</h1>}/>
+                <Route exact
+                       path={"/palette/:id"}
+                       render={routeProps => <Palette palette={generatePalette(
+                           this.findPalette(routeProps.match.params.id)
+                       )}/>}
+                />
             </Switch>
-            //  <div>
-            //     <Palette palette={generatePalette(seedColors[4])}/>
-            //  </div>
         );
     }
 }
