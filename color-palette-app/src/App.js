6901a78c8e652b2d631194e1622ec22f241a5066
7-Zip <mattkorn129@gmail.com>
2019-06-09 14:40:09 -0400

    Added SingleColorPalette to display shades of a certain color

--- color-palette-app/src/App.js
+++ color-palette-app/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Palette from './Palette';
 import PaletteList from './PaletteList';
+import SingleColorPalette from './SingleColorPalette';
 import seedColors from './seedColors';
 import {generatePalette} from './ColorHelpers';
 
@@ -16,7 +17,7 @@ class App extends Component {
         return (
             <Switch>
                 <Route exact path={"/"}
-                       render={(routeProps) => (
+                       render={routeProps => (
                            <PaletteList palettes={seedColors} {...routeProps}/>)}
                 />
                 <Route exact
@@ -26,7 +27,7 @@ class App extends Component {
                        )}/>}
                 />
                 <Route exact path={"/palette/:paletteId/:colorId"}
-                       render={() => <h1>SINGLE COLOR PAGE!</h1>}
+                       render={() => <SingleColorPalette/>}
                 />
             </Switch>
         );
