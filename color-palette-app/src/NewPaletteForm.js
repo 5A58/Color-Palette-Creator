6257fd80dab2807b15c11a38f3dd60668fad5131
7-Zip <mattkorn129@gmail.com>
2019-06-17 19:29:58 -0400

    Add ChromePicker and Buttons to drawer

--- color-palette-app/src/NewPaletteForm.js
+++ color-palette-app/src/NewPaletteForm.js
@@ -1,17 +1,19 @@
-import React, {Component} from "react";
-import classNames from "classnames";
-import {withStyles} from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import React, {Component} from 'react';
+import classNames from 'classnames';
+import {withStyles} from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Button from '@material-ui/core/Button';
+import {ChromePicker} from 'react-color';
 
-const drawerWidth = 240;
+const drawerWidth = 400;
 
 // adopted from https://material-ui.com/components/drawers/#persistent-drawer
 const styles = theme => ({
@@ -55,7 +57,7 @@ const styles = theme => ({
     },
     content: {
         flexGrow: 1,
-        padding: theme.spacing.unit * 3,
+        padding: theme.spacing(3),
         transition: theme.transitions.create("margin", {
             easing: theme.transitions.easing.sharp,
             duration: theme.transitions.duration.leavingScreen
@@ -126,6 +128,22 @@ class NewPaletteForm extends Component {
                         </IconButton>
                     </div>
                     <Divider />
+                    <Typography variant={"h4"}>Design Your Palette</Typography>
+                    <div>
+                        <Button variant={"contained"} color={"secondary"}>
+                            Clear Palette
+                        </Button>
+                        <Button variant={"contained"} color={"primary"}>
+                            Random Color
+                        </Button>
+                    </div>
+                    <ChromePicker
+                        color={"purple"}
+                        onChangeComplete={newColor => console.log(newColor)}
+                    />
+                    <Button variant={"contained"} color={"primary"}>
+                        Add Color
+                    </Button>
                 </Drawer>
                 <main
                     className={classNames(classes.content, {
