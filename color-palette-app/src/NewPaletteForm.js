dfe051c962a0f0cc07b60e6539cf8f9ca9489f1a
7-Zip <mattkorn129@gmail.com>
2019-06-20 19:23:21 -0400

    Add ability to save new palettes

--- color-palette-app/src/NewPaletteForm.js
+++ color-palette-app/src/NewPaletteForm.js
@@ -88,15 +88,16 @@ class NewPaletteForm extends Component {
         this.updateCurrentColor = this.updateCurrentColor.bind(this);
         this.addNewColor = this.addNewColor.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidMount() {
-        ValidatorForm.addValidationRule('isColorNameUnique', (value) => (
+        ValidatorForm.addValidationRule("isColorNameUnique", (value) => (
            this.state.colors.every(
                ({name}) => name.toLowerCase() !== value.toLowerCase()
            )
         ));
-        ValidatorForm.addValidationRule('isColorUnique', (value) => (
+        ValidatorForm.addValidationRule("isColorUnique", (value) => (
             this.state.colors.every(
                 ({color}) => color !== this.state.currentColor
             )
@@ -124,6 +125,17 @@ class NewPaletteForm extends Component {
         this.setState({newName: evt.target.value})
     }
 
+    handleSubmit() {
+        let newName = "New Test Palette";
+        const newPalette = {
+            paletteName: newName,
+            id: newName.toLowerCase().replace(/ /g, "-"),
+            colors: this.state.colors
+        };
+        this.props.savePalette(newPalette);
+        this.props.history.push("/");
+    }
+
     render() {
         const {classes} = this.props;
         const {open} = this.state;
@@ -132,29 +144,33 @@ class NewPaletteForm extends Component {
             <div className={classes.root}>
                 <CssBaseline />
                 <AppBar
-                    position='fixed'
+                    position="fixed"
+                    color={"default"}
                     className={classNames(classes.appBar, {
                         [classes.appBarShift]: open
                     })}
                 >
                     <Toolbar disableGutters={!open}>
                         <IconButton
-                            color='inherit'
-                            aria-label='Open drawer'
+                            color="inherit"
+                            aria-label="Open drawer"
                             onClick={this.handleDrawerOpen}
                             className={classNames(classes.menuButton, open && classes.hide)}
                         >
                             <MenuIcon />
                         </IconButton>
-                        <Typography variant='h6' color='inherit' noWrap>
+                        <Typography variant="h6" color="inherit" noWrap>
                             Persistent drawer
                         </Typography>
+                        <Button variant={"contained"} color={"primary"} onClick={this.handleSubmit}>
+                            Save Palette
+                        </Button>
                     </Toolbar>
                 </AppBar>
                 <Drawer
                     className={classes.drawer}
-                    variant='persistent'
-                    anchor='left'
+                    variant="persistent"
+                    anchor="left"
                     open={open}
                     classes={{
                         paper: classes.drawerPaper
