d802572761397c456958c685d8f09e921e939a7f
7-Zip <mattkorn129@gmail.com>
2019-06-24 18:22:52 -0400

    Implement drag and drop for ColorBoxes in NewPaletteForm

--- color-palette-app/src/DraggableColorBox.js
+++ color-palette-app/src/DraggableColorBox.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {SortableElement} from 'react-sortable-hoc'
 import {withStyles} from '@material-ui/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
 
@@ -33,7 +34,7 @@ const styles = {
     }
 };
 
-function DraggableColorBox(props) {
+const DraggableColorBox = SortableElement(props => {
     const {classes, handleClick, name, color} = props;
     return (
         <div className={classes.root}
@@ -46,6 +47,6 @@ function DraggableColorBox(props) {
             </div>
         </div>
     )
-}
+});
 
 export default withStyles(styles)(DraggableColorBox);
