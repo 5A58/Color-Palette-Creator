e7f7f52642646d7c8e497f46fa6e5350d215fefa
7-Zip <mattkorn129@gmail.com>
2019-07-05 19:34:15 -0400

    Make PaletteList responsive

--- color-palette-app/src/styles/sizes.js
+++ color-palette-app/src/styles/sizes.js
@@ -5,10 +5,11 @@ export default {
     // create a media query for all screen sizes equivalent to or smaller than size
     down(size) {
         const sizes = {
-          xs: "575.98px",
-          sm: "767.98px",
-          md: "991.98px",
-          lg: "1199.98px"
+            xs: "575.98px",
+            sm: "767.98px",
+            md: "991.98px",
+            lg: "1199.98px",
+            xl: "1600px"
         };
         return `@media (max-width: ${sizes[size]})`;
     }
