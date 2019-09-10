7dfeb874297ab5b8751d53316afb70ce4722e00a
7-Zip <mattkorn129@gmail.com>
2019-09-10 18:37:20 -0400

    Updates

---
+++ precache-manifest.4344b4d7dbc308dab722d2a06a11c116.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7b2a98f209fc7d5ad33ee2c318a769",
+    "url": "/Color-Palette-Creator/index.html"
+  },
+  {
+    "revision": "abb37730902f18286ed2",
+    "url": "/Color-Palette-Creator/static/css/2.e8e75e1a.chunk.css"
+  },
+  {
+    "revision": "78863ba66b1b9715c59e",
+    "url": "/Color-Palette-Creator/static/css/main.1d61ad5d.chunk.css"
+  },
+  {
+    "revision": "abb37730902f18286ed2",
+    "url": "/Color-Palette-Creator/static/js/2.ae4456f8.chunk.js"
+  },
+  {
+    "revision": "78863ba66b1b9715c59e",
+    "url": "/Color-Palette-Creator/static/js/main.8e970245.chunk.js"
+  },
+  {
+    "revision": "cd7e62d93ec01107191d",
+    "url": "/Color-Palette-Creator/static/js/runtime~main.dbf23445.js"
+  },
+  {
+    "revision": "67decdcf761f5faed0f6b1fa6a5d2b51",
+    "url": "/Color-Palette-Creator/static/media/bg.67decdcf.svg"
+  }
+]);
